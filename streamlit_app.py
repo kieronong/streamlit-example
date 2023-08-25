8eae830a2adc24d5c2f835e16a1229ca30783497
Kieron Ong <kieron.ong@gmail.com>
2023-08-24 21:43:08 -0700

    heas

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title('Yoghurt Park: Froyo Orders in 2018')
 
 def load_data(nrows):
     data = pd.read_csv('yopo_2015.csv', nrows=nrows)
-    data['Date'] = pd.to_datetime(data['Date'])
+    # data['Date'] = pd.to_datetime(data['Date'])
     data = data.set_index(['Date'])
     return data
 
